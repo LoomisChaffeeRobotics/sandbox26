36747f6df9dcfee21f7ec69996326170521e4494
ko-ey <ko.eyisi@gmail.com>
2025-09-11 16:43:32 -0400

    Create KosiSlide.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HAXRobotServosnStuff/KosiSlide.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.HAXRobotServosnStuff;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@TeleOp
+public class KosiSlide extends OpMode {
+
+    DcMotor slide;
+
+    @Override
+    public void init() {
+        slide = hardwareMap.get(DcMotor.class, "slide");
+
+        slide.setDirection(DcMotorSimple.Direction.REVERSE);
+        slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+    @Override
+    public void loop() {
+        if (gamepad1.dpad_up) {
+            slide.setPower(1.0);
+        }
+        else if (gamepad1.dpad_down) {
+            slide.setPower(-1.0);
+        }
+        else {
+            slide.setPower(0);
+        }
+    }
+}
