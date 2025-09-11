b0fa2e84a1af25c90961ad65a62b47c92a4a981e
liyamacode <liyamacode@gmail.com>
2025-09-11 16:40:19 -0400

    Create Liya.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HAXRobotServosnStuff/Liya.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.HAXRobotServosnStuff;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class Liya extends OpMode{
+
+    DcMotor slide;
+
+
+    @Override
+    public void init() {
+       slide=hardwareMap.get(DcMotor.class, "slide");
+       slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+       slide.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            slide.setPower(1.0);
+        }
+        else if (gamepad1.b) {
+            slide.setPower(-1.0);
+        }
+        else{
+            slide.setPower(0);
+        }
+    }
+}
