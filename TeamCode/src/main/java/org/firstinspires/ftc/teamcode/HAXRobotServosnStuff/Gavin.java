38b4182c53e8e258a6e44cc3790a0100d3c98f94
Gavintool <Gavintool@163.com>
2025-09-12 16:25:09 -0400

    Update Gavin.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HAXRobotServosnStuff/Gavin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HAXRobotServosnStuff/Gavin.java
@@ -4,16 +4,20 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class Gavin extends OpMode {
     DcMotor coil;
+    Servo sv;
 
     @Override
     public void init(){
         coil = hardwareMap.get(DcMotor.class, "slide");
         coil.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         coil.setDirection(DcMotorSimple.Direction.REVERSE);
+
     }
 
     @Override
